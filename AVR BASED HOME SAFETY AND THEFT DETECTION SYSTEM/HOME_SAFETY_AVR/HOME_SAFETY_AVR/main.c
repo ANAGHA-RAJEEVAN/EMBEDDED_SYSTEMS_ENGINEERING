21d41658b927ef6543fe3415aada553b4c1d2d2c
ANAGHA R <anagharajeevan596@gmail.com>
2026-03-08 16:49:17 +0530

    main.c updated

--- AVR BASED HOME SAFETY AND THEFT DETECTION SYSTEM/HOME_SAFETY_AVR/HOME_SAFETY_AVR/main.c
+++ AVR BASED HOME SAFETY AND THEFT DETECTION SYSTEM/HOME_SAFETY_AVR/HOME_SAFETY_AVR/main.c
@@ -10,149 +10,117 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-
 #include "adc.h"
 #include "UART.h"
 
+volatile int flag = 0;
 
+void init_gpio(void);
+void init_pir(void);
+void detect_fire(void);
+void detect_motion(void);
 
-//Pin Configuration
-
-#define BUZZER_PORT PORTB
-#define BUZZER_PIN  PORTB0
-#define BUZZER_DDR  DDRB
-#define BUZZER_REG  DDB0
-
-#define LED_PORT    PORTB
-#define LED_PIN     PORTB1
-#define LED_DDR     DDRB
-#define LED_REG     DDB1
-
-
-#define PIR_DDR     DDRD
-#define PIR_REG     DDD2
-#define PIR_PINREG  PIND
-#define PIR_PIN     PIND2      // INT0 input
-
-
-volatile uint8_t intrusion_flag = 0;
-
-//Function prototype
-void GPIO_init(void);  
-void PIR_init(void);
-void fire_detection(void);
-void intrusion_detection(void);
-
-//ISR function called when interrupt is triggered
+//Interrupt service routine
 ISR(INT0_vect)
 {
-    intrusion_flag = 1;
+	flag = 1;
 }
 
-int main(void)
+int main()
 {
-    GPIO_init();	//Initialize GPIO
-    adc_init();		//Initialize ADC
-    UART_init();	//Initialize UART
-    PIR_init();		//Initialize PIR
-    sei();			//Enable Interrupt
-
-    // PIR warm-up time
-    _delay_ms(30000);
-
-	//System on after warm up time
-    UART_send_string("SYSTEM ON\n");
-
-    while (1)
-    {
-        intrusion_detection();  //Intrusion detection function
-		fire_detection();		//Fire detection function
-        _delay_ms(200);
-    }
-}
-
-
-//GPIO Initialization
-void GPIO_init(void)
-{
-    BUZZER_DDR |= (1 << BUZZER_REG);
-    LED_DDR    |= (1 << LED_REG);
-
-    BUZZER_PORT &= ~(1 << BUZZER_PIN);
-    LED_PORT    &= ~(1 << LED_PIN);
-}
-
-
-//PIR Initialization
-void PIR_init(void)
-{
-    PIR_DDR &= ~(1 << PIR_REG);    // PD2 as input
-    PORTD &= ~(1 << PIR_PIN);      // no pull-up
-
-    // INT0 Rising Edge Trigger
-    EICRA |= (1 << ISC01) | (1 << ISC00);
-    EIMSK |= (1 << INT0);
-}
-
-
-
-//Fire detection
-void fire_detection(void)
-{
-    unsigned int adc_value;
-    int temp;
-	//Read adc value
-    adc_value = adc_read(0);   
-	//Converting adc value to Temperature          
-    temp = (int)(adc_value * 0.488);      
+	init_gpio(); //Configure GPIO  
+	adc_init();	//Configure ADC		
+	UART_init();//Configure UART
+	init_pir();	//Configure PIR
+	sei();//Enable global interrupt
 	
-	//If temp > 60, fire hazard is detected
-    if (temp > 60)
-    {
-        // Warning light ON
-        LED_PORT |= (1 << LED_PIN);
-		//Buzzer ON
-        BUZZER_PORT |= (1 << BUZZER_PIN);
-
-        UART_send_string("FIRE ALERT!\n");
-        _delay_ms(1000);
-
-        BUZZER_PORT &= ~(1 << BUZZER_PIN);
-    }
-    else
-    {
-        LED_PORT &= ~(1 << LED_PIN);
-    }
+	//PIR takes around 30 seconds to warmup so we should give a delay of 30seconds
+	_delay_ms(30000);
+	
+	//System turns on after warmup
+	UART_send_string("SYSTEM ON\n");
+	//UART_send_string("debug correct\n");
+	
+	while(1)
+	{
+		detect_motion();			//Checking if pir triggered
+		detect_fire();	//Checking fire hazard
+		_delay_ms(200);				//200ms delay
+	}
 }
 
-
-//Intrusion detection
-void intrusion_detection(void)
+//Setup buzzer and led
+void init_gpio(void)
 {
-    if (intrusion_flag)
-    {
-        intrusion_flag = 0;
+	//PB0(BUZZER) and PB1(LED) is set as outut
+	DDRB |= (1 << DDB0) | (1 << DDB1);  
+	//Initially reset both buzzer and led
+	PORTB &= ~(1 << PORTB0); //buzzer
+	PORTB &= ~(1 << PORTB1); //led
+	
+}
 
-        _delay_ms(50);
+//Setup PIR motion sensor
+void init_pir(void)
+{
+	//PD2(MOTION SENSOR) is set as input
+	DDRD &= ~(1 << DDD2);
+	
+	//Interrupt enabling 
+	EIMSK |= (1 << INT0); //Enabling interrupt 0.
+	EICRA |= (1 << ISC01) | (1 << ISC00); //Rising edge trigger for interrupt
+}
+
+//Fire hazard detection
+void detect_fire(void)
+{
+	unsigned int adc_value;
+	int tempvalue = 0; //temperature value
+	
+	adc_value = adc_read(0); //Read anlog value from LM35 that's from channel 0
+	tempvalue = (int)(adc_value * 0.488); //Convert analog value to temperature
+	
+	//Buzzer will on if temp goes beyond 60
+	if (tempvalue > 60)
+	{
+		PORTB |= (1 << PORTB1); //led will be turned ON
+		PORTB |= (1 << PORTB0); //Buzzer will be turned ON
 		
-		//If PIR_PIN is high,Intrusion detected
-        if (PIR_PINREG & (1 << PIR_PIN))
-        {
-            UART_send_string("INTRUSION DETECTED!\n");
+		//Printing message on uart
+		UART_send_string("FIRE ALERT!\n");
+		//Keep buzzer on for 1 second
+		_delay_ms(1000);
+		PORTB &= ~(1 << PORTB0); //buzzer will be turned off
+	}
+	else
+	{
+		PORTB &= ~(1 << PORTB1); //Led will be turned off
+	}
+}
+
+//Motion detection
+void detect_motion(void)
+{
+	if (flag == 1)
+	{
+		flag = 0;
+		_delay_ms(100);
+		
+		if (PIND & (1 << PIND2))
+		{
+			//Printing message on uart
+			UART_send_string("INTRUSION DETECTED!\n");
+			PORTB |= (1 << PORTB1); //led ON
+			//buzzer alarm pattern
+			PORTB |= (1 << PORTB0); //Buzzer will be turned ON
+			_delay_ms(500);
+			PORTB &= ~(1 << PORTB0);   //buzzer will be turned off
+			_delay_ms(100);
+			PORTB |= (1 << PORTB0);//Buzzer will be turned ON
+			_delay_ms(500);
+			PORTB &= ~(1 << PORTB0);  //buzzer will be turned off
 			
-			//Warning light ON
-			LED_PORT |= (1 << LED_PIN);
-			
-            // Alarm pattern
-            BUZZER_PORT |= (1 << BUZZER_PIN);
-            _delay_ms(500);
-            BUZZER_PORT &= ~(1 << BUZZER_PIN);
-            _delay_ms(200);
-            BUZZER_PORT |= (1 << BUZZER_PIN);
-            _delay_ms(500);
-            BUZZER_PORT &= ~(1 << BUZZER_PIN);
-			
-            _delay_ms(2000);
-        }
-    }
+			_delay_ms(2000); //delay to prevent continuous alarm
+		}
+	}
 }
