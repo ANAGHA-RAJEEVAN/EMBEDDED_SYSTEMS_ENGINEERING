944466c5302d4a66bf5e09f3871bb7bacf8742c5
ANAGHA R <anagharajeevan596@gmail.com>
2026-05-13 22:18:35 +0530

    Added question 12 (palindrome) solution.

---
+++ C_ASSIGNMENT/QUESTION_12_PALINDROME.c
@@ -0,0 +1,35 @@
+/*
+    AUTHOR : ANAGHA R
+    DATE   : 11/05/2026
+    PROGRAM TO CHECK WHETHER A STRING IS A PALINDROME OR NOT.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char string[100];
+    int flag = 0;
+    printf("Enter a string: ");
+    scanf("%s", string);
+
+    int length = strlen(string);
+    for(int i =0; i <length/2; i++)
+    {
+        if(string[i] != string[length-i-1])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 0)
+    {
+        printf("The string is a palindrome.\n");
+    }
+    else
+    {
+        printf("The string is not a palindrome.\n");
+    }
+    return 0;
+}
