73f3261b21b786fcea73dd20b063c826f15e3984
ANAGHA R <anagharajeevan596@gmail.com>
2026-05-15 21:55:28 +0530

    Added question 18 (extract bits) solution.

---
+++ C_ASSIGNMENT/QUESTION_18_EXTRACT_BITS.c
@@ -0,0 +1,26 @@
+/*
+    AUTHOR : ANAGHA R
+    DATE   : 14/05/2026
+    PROGRAM TO EXTRACT BITS FROM A SPECIFIC POSITION. 
+*/
+
+#include <stdio.h>
+int main()
+{
+    int num,position,n;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    printf("Enter the starting position : ");
+    scanf("%d", &position);
+
+    printf("Enter the number of bits : ");
+    scanf("%d", &n);
+
+    int mask = (1 << n)-1;  //mask
+    int result = (num >> position) & mask; // shift to right to extract the bits 
+
+    printf("Extracted bits: %d\n", result);
+    return 0;
+}
