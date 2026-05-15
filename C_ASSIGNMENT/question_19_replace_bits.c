715f5eb51dde6552f7d553d67b488b2ffa462665
ANAGHA R <anagharajeevan596@gmail.com>
2026-05-15 22:15:11 +0530

    Added question 19 (replace bits) solution.

---
+++ C_ASSIGNMENT/question_19_replace_bits.c
@@ -0,0 +1,32 @@
+/*
+    AUTHOR : ANAGHA R
+    DATE   : 14/05/2026
+    PROGRAM TO REPLACE BITS IN A NUMBER FROM ANOTHER.
+*/
+
+#include <stdio.h>
+int main() 
+{
+    int num1,num2,position,n;
+
+    printf("Enter the first number : ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number : ");
+    scanf("%d", &num2);
+
+    printf("Enter the starting position : ");
+    scanf("%d", &position);
+
+    printf("Enter the number of bits : ");
+    scanf("%d", &n);
+
+    int mask = (1 << n)-1;
+    num1 &= ~(mask << position); // Clear n bits
+
+     num2 = (num2 & mask) << position; // extract  n bits from num2 
+    num1 |= num2; // replace buts in num1 from num2
+
+    printf("The resulting number is : %d\n", num1);
+    return 0;
+}
