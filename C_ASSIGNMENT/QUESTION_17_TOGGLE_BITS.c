55448afd63ada9ac8d7ae623b312c69d3897a4ef
ANAGHA R <anagharajeevan596@gmail.com>
2026-05-15 21:45:12 +0530

    Added question 17 (toggle bits) solution.

---
+++ C_ASSIGNMENT/QUESTION_17_TOGGLE_BITS.c
@@ -0,0 +1,27 @@
+/*
+    AUTHOR : ANAGHA R
+    DATE   : 14/05/2026
+    PROGRAM TO TOGGLE BITS IN A NUMBER From a position. 
+*/
+
+#include <stdio.h>
+int main()
+{
+    int number, position, n;
+    printf("Enter a number : ");  
+    scanf("%d", &number);
+    printf("Enter the starting bit position : ");  //Enter the starting bit positio
+    scanf("%d", &position);
+    printf("Enter the number of bits to toggle : "); //Enter the number of bits to toggle
+    scanf("%d", &n);
+
+    
+    int mask = ((1 << n) - 1);  // Create a mask 
+    mask <<= position; // Shift the mask to the correct starting position
+
+    // Toggling bits
+    number ^= mask;
+
+    printf("Number after toggling bits: %d\n", number);
+    return 0;
+}
