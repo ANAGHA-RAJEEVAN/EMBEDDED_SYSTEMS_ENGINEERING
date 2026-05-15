8f562f280303a27951d4a36648cecb495bb0d5c6
ANAGHA R <anagharajeevan596@gmail.com>
2026-05-15 23:28:43 +0530

    Added question 19 (replace bits) solution.

--- C_ASSIGNMENT/question_19_replace_bits.c
+++ C_ASSIGNMENT/question_19_replace_bits.c
@@ -22,9 +22,9 @@ int main()
     scanf("%d", &n);
 
     int mask = (1 << n)-1;
-    num1 &= ~(mask << position); // Clear n bits
+    num1 &= ~(mask << position-n+1); // Clear n bits
 
-     num2 = (num2 & mask) << position; // extract  n bits from num2 
+     num2 = (num2 & mask) << position-n+1; // extract  n bits from num2 
     num1 |= num2; // replace buts in num1 from num2
 
     printf("The resulting number is : %d\n", num1);
