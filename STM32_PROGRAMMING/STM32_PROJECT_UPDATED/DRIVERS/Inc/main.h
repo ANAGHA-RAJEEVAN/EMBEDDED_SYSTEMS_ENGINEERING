f804262534cb0f8060e9a93aa3807c4ba0543e7e
ANAGHA R <anagharajeevan596@gmail.com>
2026-06-20 00:34:20 +0530

    Added main.h.

---
+++ STM32_PROGRAMMING/STM32_PROJECT_UPDATED/DRIVERS/Inc/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Jun 20, 2026
+ *      Author: anagh
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#define GPIO_APP    0
+#define USART_APP   0
+#define SPI_APP     0
+#define I2C_APP     1
+
+
+#endif /* INC_MAIN_H_ */
